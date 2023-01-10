86aa91b2937931dca263326cce48590ad1a20fac
Priyang <patelpriyang95@gmail.com>
2023-01-10 23:21:54 +0530

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import { useState } from "react";
+import Navbar from "./Components/Navbar";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App bg-red-500">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="Ap">
+      <Navbar />
     </div>
   );
 }
